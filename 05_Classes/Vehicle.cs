a1e6bcef5c301abb89c2c912c6bbb5be25bbf637
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2021-08-04 16:26:09 -0400

    Indicators

--- 05_Classes/Vehicle.cs
+++ 05_Classes/Vehicle.cs
@@ -9,6 +9,24 @@ namespace _05_Classes
     public enum VehicleType { Car, Truck, Van, Spaceship, Plane}
     public class Vehicle
     {
+        // Hidden implicit constructor
+        // Needs to be manually written in order to be accesse
+        // If there is any other constructor present
+        public Vehicle() { }
+
+        public Vehicle(string make, string model, double mileage, VehicleType type)
+        {
+            Make = make;
+            Model = model;
+            Mileage = mileage;
+            TypeOfVehicle = type;
+            LeftIndicator = new Indicator();
+            RightIndicator = new Indicator();
+        }
+
+
+
+
         //1 Access modifier => Where can this be seen?
         //2 Type => Type of the property as a held value
         //3 Name => Name of the property method, Pascal case
@@ -19,5 +37,66 @@ namespace _05_Classes
         public string Model { get; set; }
         public double Mileage { get; set; }
         public VehicleType TypeOfVehicle { get; set; }
+        public bool IsRunning { get; private set; }
+
+        public void TurnOn()
+        {
+            IsRunning = true;
+
+            Console.WriteLine("You turn on the vehicle.");
+        }
+
+        public void TurnOff()
+        {
+            IsRunning = false;
+
+            Console.WriteLine("You turn off the vehicle.");
+        }
+
+        public Indicator RightIndicator { get; set; }
+        public Indicator LeftIndicator { get; set; }
+
+        public void TurnRight()
+        {
+            RightIndicator.TurnOn();
+            LeftIndicator.TurnOff();
+        }
+
+        public void TurnLeft()
+        {
+            LeftIndicator.TurnOn();
+            RightIndicator.TurnOff();
+        }
+
+        public void TurnOnHazards()
+        {
+            LeftIndicator.TurnOn();
+            RightIndicator.TurnOn();
+        }
+
+        public void ClearIndicators()
+        {
+            LeftIndicator.TurnOff();
+            RightIndicator.TurnOff();
+        }
+    }
+
+    public class Indicator
+    {
+        // Create a class called Indicator that has a property called IsFlashing
+        // IsFlashing should have a private set
+        // The Indicator class also then needs the methods required to change the state of the IsFlashing Property
+
+        public bool IsFlashing { get; private set; }
+
+        public void TurnOn()
+        {
+            IsFlashing = true;
+        }
+
+        public void TurnOff()
+        {
+            IsFlashing = false;
+        }
     }
 }
--- 05_Classes/VehicleTests.cs
+++ 05_Classes/VehicleTests.cs
@@ -22,5 +22,54 @@ namespace _05_Classes
             Console.WriteLine($"My car is a {firstVehicle.Make} {firstVehicle.Model} with {firstVehicle.Mileage} miles on it.");
 
         }
+
+        [TestMethod]
+        public void MethodsTests()
+        {
+            Vehicle transport = new Vehicle();
+            transport.TurnOn();
+            Assert.IsTrue(transport.IsRunning);
+            transport.TurnOff();
+        }
+
+        [TestMethod]
+        public void TurnSignalTests()
+        {
+            Vehicle myCar = new Vehicle();
+            myCar.TurnOn();
+            myCar.LeftIndicator = new Indicator();
+            myCar.RightIndicator = new Indicator();
+
+            myCar.TurnRight();
+            Console.WriteLine("Turning right now");
+
+            CheckIndicators(myCar);
+
+            myCar.TurnLeft();
+            Console.WriteLine("Turning left");
+            
+            CheckIndicators(myCar);
+        }
+
+        public void CheckIndicators(Vehicle myCar)
+        {
+            Console.WriteLine($"Right Indicator: {myCar.RightIndicator.IsFlashing}");
+            Console.WriteLine($"Left Indicator: {myCar.LeftIndicator.IsFlashing}");
+        }
+
+        [TestMethod]
+        public void Constructors()
+        {
+            // old way, ew gross icky
+            Vehicle car = new Vehicle();
+            car.Make = "Toyota";
+            car.Model = "Corolla";
+            car.Mileage = 2119021;
+
+            Vehicle rocket = new Vehicle("SpaceX", "Falcon Heavy", 300000, VehicleType.Spaceship);
+            Console.WriteLine(rocket.Make);
+            Console.WriteLine(rocket.Model);
+
+        }
     }
 }
