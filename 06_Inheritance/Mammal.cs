83d0117b333e3ae27e70ae924f0d8c7ce3014141
AndroidDoctorr <gbartthenerd@gmail.com>
2021-08-05 12:05:08 -0400

    added some inheritance examples

---
+++ 06_Inheritance/Mammal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Inheritance
+{
+    public class Mammal : Animal
+    {
+        public string FurColor { get; set; }
+        public Mammal()
+        {
+            HasFur = true;
+            NumberOfLegs = 4;
+            Console.WriteLine("This is the Mammal constructor");
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine("ROAR!");
+        }
+
+    }
+
+    public class Dog : Mammal
+    {
+        public bool HasFloppyEars { get; set; }
+        public Dog()
+        {
+            
+        }
+    }
+
+    public class Reptile : Animal
+    {
+        public Reptile()
+        {
+            
+        }
+    }
+}
