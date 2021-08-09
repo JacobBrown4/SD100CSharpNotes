f043c68a6153836516984ba82b103912b49171e5
AndroidDoctorr <gbartthenerd@gmail.com>
2021-08-09 10:56:31 -0400

    added SC test, SC refactor

---
+++ 07_RepositoryPattern_Tests/StreamingContentTests.cs
@@ -0,0 +1,41 @@
+﻿using _07_RepositoryPattern_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _07_RepositoryPattern_Tests
+{
+    [TestClass]
+    public class StreamingContentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            StreamingContent movie = new StreamingContent();
+            Console.WriteLine($"{movie.Title} is a {movie.GenreType} movie about {movie.Description} rated {movie.MaturityRating}");
+        }
+
+        [TestMethod]
+        public void SetTitle_ShouldSetCorrectString()
+        {
+            // AAA Pattern
+            
+            // Arrange
+            StreamingContent content = new StreamingContent();
+
+            // Act
+            content.Title = "Double Down";
+
+            // Assert
+            string expected = "DOUBLE DOWN";
+            string actual = content.Title;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SetMaturityRating_ShouldGetCorrectFamilyFriendly()
+        {
+
+        }
+    }
+}
