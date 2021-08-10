bb8ff296e18749976394eb14c318b9bb865cfb8f
AndroidDoctorr <gbartthenerd@gmail.com>
2021-08-10 16:01:50 -0400

    repo console methods

--- 09_StreamingContent_Console/UI/ProgramUI.cs
+++ 09_StreamingContent_Console/UI/ProgramUI.cs
@@ -1,4 +1,5 @@
-﻿using _08_StreamingContent_Inheritance;
+﻿using _07_RepositoryPattern_Repository;
+using _08_StreamingContent_Inheritance;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -83,6 +84,116 @@ namespace _09_StreamingContent_Console.UI
         public void AddNewContent()
         {
             Console.Clear();
+
+            StreamingContent content = new StreamingContent();
+
+            // Title
+            bool isValidTitle = false;
+            while (!isValidTitle)
+            {
+                Console.Write("Title: ");
+                string title = Console.ReadLine();
+
+                // content.Title = string.IsNullOrWhiteSpace(title) ? "Untitled" : title;
+                if (string.IsNullOrWhiteSpace(title))
+                {
+                    Console.WriteLine("Please enter a valid title (press any key to continue)");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    content.Title = title;
+                    isValidTitle = true;
+                }
+            }
+
+            // Description
+            Console.Write("Description: ");
+            string description = Console.ReadLine();
+            content.Description = string.IsNullOrWhiteSpace(description) ? "(No Description)" : description;
+
+            // Star Rating
+            Console.Write("Star Rating (0-5): ");
+            double stars = double.Parse(Console.ReadLine());
+            if (stars > 5)
+            {
+                content.StarRating = 5;
+            } else if (stars < 0)
+            {
+                content.StarRating = 0;
+            } else
+            {
+                content.StarRating = stars;
+            }
+
+            // Maturity
+            Console.WriteLine("1. G\n" +
+                "2. PG\n" +
+                "3. PG-13\n" +
+                "4. R\n" +
+                "5. NC-17\n" +
+                "6. TV-MA\n" +
+                "7. TV-G\n" +
+                "8. TV-Y\n");
+            Console.Write("Maturity rating (#): ");
+            string maturityInput = Console.ReadLine();
+            int maturityId = int.Parse(maturityInput);
+            content.MaturityRating = (MaturityRating)maturityId;
+
+            // content.MaturityRating = (MaturityRating)Int32.Parse(Console.ReadLine());
+
+            // compile-time error gives you a red line, won't build or run
+            // run-time error doesn't happen until the code builds and runs
+
+            // Genre
+            Console.WriteLine("1. SciFi\n" +
+                "2. Comedy\n" +
+                "3. Horror\n" +
+                "4. Romantic Comedy\n" +
+                "5. Documentary\n" +
+                "6. Western\n" +
+                "7. Action");
+            Console.Write("Genre: ");
+            string genreInput = Console.ReadLine();
+
+            switch(genreInput)
+            {
+                case "scifi":
+                case "SciFi":
+                case "sci-fi":
+                case "Sci-Fi":
+                case "one":
+                case "1":
+                    content.GenreType = GenreType.SciFi;
+                    break;
+                case "2":
+                case "comedy":
+                    content.GenreType = GenreType.Comedy;
+                    break;
+                case "3":
+                case "horror":
+                    content.GenreType = GenreType.Horror;
+                    break;
+                case "4":
+                case "rom com":
+                case "romantic comedy":
+                    content.GenreType = GenreType.RomCom;
+                    break;
+                case "5":
+                    content.GenreType = GenreType.Documentary;
+                    break;
+                case "6":
+                    content.GenreType = GenreType.Western;
+                    break;
+                case "7":
+                    content.GenreType = GenreType.Action;
+                    break;
+                default:
+                    content.GenreType = 0;
+                    break;
+            }
+
+            _repo.AddContentToDirectory(content);
         }
     }
 }
